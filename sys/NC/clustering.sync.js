5acca4875ee44e314103923a47bc8afcbbf3009d
iki1uc <entertrainer@web.de>
2026-08-28 03:47:44 +0200

    Create clustering.sync.js

---
+++ sys/NC/clustering.sync.js
@@ -0,0 +1,12 @@
+/tmp.lauf/
+    boot.sync
+    quick.drive
+    system.ready
+    NEXT.tmp
+    sys.tmp
+    chromium.os   ← Kernel (zeigt nur an)
+
+/sys/NC/
+    run3.js
+    kohärenz.axiom.js
+    clustering.sync.js   ← HIER kommt es rein
